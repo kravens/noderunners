6b7de719feedabf43fc25bd9ee0c7149985f9f33
Kevin Ravensberg <kevinravensberg@gmail.com>
2026-06-20 10:52:10 +0200

    ASCII art version of the logo
    In a hidden python script, which also looks like the logo.

---
+++ noderunners_art.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# NODERUNNERS - this source IS the logo. The disk below is the program,
+# zlib+base85-packed; the carved-out "N" and its node-leads are blank space.
+# Strip the whitespace, unpack, and run. Functionally identical. CC0.
+import base64 as _b,zlib as _z
+_=r"""
+
+
+
+
+                                                   c-pO
+                                        3YjWE-68_Iq^wO0nKzxX#Em;wT
+                                  Y&nU?_Kc&X#>u+26qSn)km8ymR3IcvH`o)*0rw
+                              =^03|<WGF$$!Oe~7s?62RA*3sYk>sI#Ds_FA)1FM}{h{ie
+                           sujfG~@~SDvuvUdPGL)mI3t$SL;Uk>GZ}1JjPtCU*c>WIJ2r!qvC
+                         Gg=x0ClBQ%|WEay3G>Bb5L9^t0t*Ii5;j#q9AFiC9+z4;znE}!FT<wuR
+                      q>bDic)$Q;~DIZW=CINH&Q|<QTP-wRk|gp+T;fSM5DKRBC~sk4PHI0D(QVAxq>
+                    2%@At^qJT=dAd{-m2w@ 99LD0IeTGy>6)YUyl4No>9R0jlw<n5x~d7GrJa^qK8i|pP
+                   <9cS0qa0O>KzrxiGe7!v  T3jh544SxGJg|p{K{qG&p&)@L<f1F)keLVYmHNAlMS6
+                 @GTzQPE9{&?=&_(Cm          8fsRU*^R_CRba(SJ5R
+                5I=NdKS9s+KDu6|}q7          6|JkPC3i6;7{wMoS)%
+              okq4<*6HicJRNAQqf>~u           mgIo{dy<_b!Yx0aH-          F@pK6$}t1J<
+             e6xSs$5G>c+*uNvrPp)sy            c$2D+6Uhtjk1-b-A          $kHsdP==jK+4
+            X1q$;*1B~sUBLz;H+t(n)A             S%K#jTtdFeO`eN5          =rKbw#VzmE&Nwv       =
+            G6U5AV^W1ib;p$ky?XL)!O              ;kp((PjClS@G}%          r(KMFudz#BdxjV4VFo<9sG
+           y9vI2@U2i-cCsoL3f3!gN8r                3lN`^q4XaHI?          m@qYAq4}PqE!jE#Ao5f%^n)
+          Rtpc>1V#mIp<Kx!~B@B7CF>2                 ySe?D*abzK1          Mh^D)+y_E{zwcBCi@<9;TjZa
+          WKB8h`8gqhs3ZMb2Igenq5ci                  0<lmJTko(O          pV*}WtCMjTi1y+EepKKv7NgF
+         %W7$X<Z3M&6H6UF9SMEcuIW2k                   U|q1;JU+(          {c>Q$*7vqwPCC3s3I3J@f$ar1
+         1R)MGzaKOl<DLJ{MV+%^Hf#Zh                    -nw>qdZ#          5gXjAVUs>xHxz>5tG4NAZpk*(
+         aATi8=e^FLo=TOxdAm#d6G|AH          )           soN--|          |WVOOL5K@_BG(th+=B{Sam>;-
+         -mOg*}ba8kn=?}bkO-z=Fyw_m          |2           C2w*(          C{s$LcsJd{H_XP-Nj7KXW4uf%
+         *WWks%q98nOb6v62uGLzzvOnA          #VOi          1S#D          tW71i>nX@qzM8*P_a&mbn_AHy
+         +KE?wzYL!N(AwChpxryiad?tI          hPBv$          ub;          D-F4+58jBlpw|D0PW{e1eZr;0
+         ^}v$PjQwkm%&p4e>7;8qz3262          1_=9lC          EL          O$z;v5}ibD6bvYxn<0{knrvP_
+         M0BWyi6cgHxdQJUNVjxT3pOm!          u5Ts*@V          e          y?nx}VR_q6HKaQ~Esnm6QzdR-
+         h7mn70nN;gR<yP4h3<J2XobW?          =xC{8%<v                    bLG9)Px>K*2YU{pQK)*`zfUHz
+          T>J!A)JE%ULt8vQP)sPq?BG#          K2cJnpmeG                   !kNtWx^Fo~q6P^OS<fPC>eqp
+          5O`N7G6z6mi0@iiM^?WgO4&F          t^nFr8i6`zH                 Hi4}ugArUQRmija0K!uG{z!D
+           D%*V-!8~h7C52pH(R=a%0k9          ;AGgMr^oaSxp                b~+oabzjd-6xQE0Vg_8$qV$
+            2P|+D^;J^U8g0XHW;%Wt^*          ;HyHE85;gvkD>               BYXZawH|9b&nMU8jl)Rj<$
+            3       >__Wa>+&pnjxc*          B;-mTX*qG^GElm              ~_k5=FAX{8J$z$dCB*1dcf
+                      !27c<@w5}V@{          N8Emu(QfJmNKfOY             w7Om`bwRPN=rt)@F#A8h|
+                       J&Be(5>!F%J          X+@U&VG9*fSz~nN#%           pk(1G%OdZ1Ww3y@{kK!n
+                                            PlXw4@Z(92eIjD291=          Y&1R32q)|6jNX5`>{n
+                                            2M{c6s?TBjFBVigHb>          U2ryEC6iso1^Aip;T
+                      Cyb3LI1gf+2XQo9+74Du52YRTLRZUd+(L0LEY@}XJZ   kBFPu0ho9ly|_rK3(mI1
+                    FMOL-8=!Yan~$T4Nk_+85gP-GyfEeMeksdaPAdT@&33xH  Vu~KD}}XlYL|o^favZ$
+                      -#er>L~iB4#eQ2@ZDX5t$cUKn0<G*#7+6`&PJ8Uu|3b2 U~3`0X(h7UjQ3W>n!
+                         vY}BIrZI^s$1cn;srUQSw}77LMn-X#J|qt5S<lS8pL$mjN)Glo)Lk;u|
+                           h45Qk{~Vs3wr2DK>w+$;5<V7LUq!=l3991UY;|0*WUSdha+>MYwY
+                              g+;w?7ig?rc>T8Ke1)gRfX0P`X9(=~%U}5}6^YER8vN-Z*
+                                  DKZSnNa9oG4*bIun0|A>=TEXpw@o@pq8L}....
+                                        ..........................
+                                                   ....
+
+
+
+
+"""
+exec(_z.decompress(_b.b85decode("".join(_.split()).replace(".",""))).decode("utf-8"))
